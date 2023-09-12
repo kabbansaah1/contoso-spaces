f456865d02198c314b6fbde96e2d5245d0931aaa
Kobina Abban-Saah <kabbansaah1@babson.edu>
2023-09-12 04:35:30 -0500

    yy

--- main.tf
+++ main.tf
@@ -48,8 +48,8 @@ resource "aws_ecs_task_definition" "app_task" {
   DEFINITION
   requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
   network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
-  memory                   = 512         # Specify the memory the container requires
-  cpu                      = 256         # Specify the CPU the container requires
+  memory                   = 512         
+  cpu                      = 256         
   execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
 }
 
