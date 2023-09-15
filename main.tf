f21d709c7f3f0363221351feaf85d81393bf23f0
Kobina Abban-Saah <kabbansaah1@babson.edu>
2023-09-15 14:40:01 -0700

    gg

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_ecs_cluster" "my_cluster" {
   name = "app-cluster" # Name your cluster here
 }
 
-# main.tf
+# main.tf hh
 resource "aws_ecs_task_definition" "app_task" {
   family                   = "app-first-task" # Name your task
   container_definitions    = <<DEFINITION
